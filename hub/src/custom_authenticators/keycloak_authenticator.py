cab1af177a2e961f3796aa232a26e9b623348331
Guy Feldman <guy.feldman@KMBPM1-GuyFeldman.local>
2023-06-08 16:49:22 -0400

    formatting

--- hub/src/custom_authenticators/keycloak_authenticator.py
+++ hub/src/custom_authenticators/keycloak_authenticator.py
@@ -1,17 +1,17 @@
 from oauthenticator.generic import GenericOAuthenticator
-# subclassing the GenericOAuthenticator in case we need to 
+
+# subclassing the GenericOAuthenticator in case we need to
 # enrich the user info
 # https://github.com/jupyterhub/oauthenticator/blob/main/oauthenticator/generic.py
 
+
 class KeycloakAuthenticator(GenericOAuthenticator):
-    login_service = 'keycloak'
+    login_service = "keycloak"
     userdata_params = {"state": "state"}
-    username_key =  "preferred_username"
+    username_key = "preferred_username"
     admin_groups = ["jupyterhub-admin"]
     allowed_groups = ["jupyterhub-user", "jupyterhub-admin"]
-    enable_auth_state =  True
+    enable_auth_state = True
 
     def claim_groups_key(self, userinfo_data):
-        return userinfo_data['jupyterhub_groups']
-
-    
+        return userinfo_data["jupyterhub_groups"]
--- hub/src/custom_kube_spawner.py
+++ hub/src/custom_kube_spawner.py
@@ -2,6 +2,7 @@ import json
 from kubespawner import KubeSpawner
 from kubernetes_asyncio.client import models as k8s
 
+
 class CustomKubeSpawner(KubeSpawner):
     def auth_state_hook(self, spawner, auth_state):
         """
@@ -19,31 +20,54 @@ class CustomKubeSpawner(KubeSpawner):
         Allowing us to dynamically generate profiles based on the user info
         """
         user_info = spawner.userinfo
-        image_options = {"display_name": "Image", "choices": {"custom": {"display_name": "Custom Single Server", "kubespawner_override": {"image": "gfeldman8/jupyterhub-k8s-singleuser:latest"} } , "pytorch2": {"display_name": "Python 3 Training Notebook 2", "kubespawner_override": {"image": "training/python:2022.01.01"} } }}
-        return [{'display_name': f'Training Env for {user_info.get("name")}', 'slug': 'training-python', 'default': True, "profile_options": {"image": image_options} }, {'display_name': f'Prod Env for {user_info.get("name")}','slug': 'prod-python','default': False}]
+        image_options = {
+            "display_name": "Image",
+            "choices": {
+                "custom": {
+                    "display_name": "Custom Single Server",
+                    "kubespawner_override": {
+                        "image": "gfeldman8/jupyterhub-k8s-singleuser:latest"
+                    },
+                },
+                "pytorch2": {
+                    "display_name": "Python 3 Training Notebook 2",
+                    "kubespawner_override": {"image": "training/python:2022.01.01"},
+                },
+            },
+        }
+        return [
+            {
+                "display_name": f'Training Env for {user_info.get("name")}',
+                "slug": "training-python",
+                "default": True,
+                "profile_options": {"image": image_options},
+            },
+            {
+                "display_name": f'Prod Env for {user_info.get("name")}',
+                "slug": "prod-python",
+                "default": False,
+            },
+        ]
 
     def get_env(self):
         env = super().get_env()
-        if hasattr(self,'userinfo'):
-            env['PROP_USER_INFO'] = self.userinfo.get("name", "")
-        if hasattr(self, 'user_options'):
-            env['PROP_USER_PROFILE'] = self.user_options.get("profile","")
+        if hasattr(self, "userinfo"):
+            env["PROP_USER_INFO"] = self.userinfo.get("name", "")
+        if hasattr(self, "user_options"):
+            env["PROP_USER_PROFILE"] = self.user_options.get("profile", "")
         self.log.info(f"Got environment variables {env}")
         return env
-    
+
     def get_service_account_from_user_info(self, spawner):
         user_info = spawner.userinfo
         self.log.info(f"got user info in modify_pod_hook {user_info}")
         return "jupyterhub-user"
-    
+
     def modify_pod_hook(self, spawner, pod: k8s.V1Pod):
         # set service account based on user info
         pod.spec.service_account = self.get_service_account_from_user_info(spawner)
         pod.spec.service_account_name = self.get_service_account_from_user_info(spawner)
         pod.spec.automount_service_account_token = True
-        # set user root to read only file system so they do not store files there permanently
-        pod.spec.volumes.append(k8s.V1Volume(name="jupyter-root",empty_dir=k8s.V1EmptyDirVolumeSource()))
-        pod.spec.containers[0].volume_mounts.append(k8s.V1VolumeMount(name="jupyter-root", mount_path="/tmp/container",read_only=False))
         pod.spec.containers[0].image_pull_policy = "Always"
         self.log.info(f"got pod in modify_pod_hook {pod}")
         return pod
